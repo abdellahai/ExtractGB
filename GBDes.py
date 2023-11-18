dfe6c07f1eb419c96579e362f3dc7d8d359d813a
Abdellah IDRISSI AZAMI <idrissi.azami.abdellah@gmail.com>
2023-11-19 00:46:30 +0100

    Update GBDes.py

--- GBDes.py
+++ GBDes.py
@@ -4,7 +4,7 @@ import pandas as pd
 import io
 from Bio.SeqUtils import GC
 from Bio.Seq import Seq
-@st.cache
+@st.cache_data
 def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
     return df.to_csv().encode('utf-8')
